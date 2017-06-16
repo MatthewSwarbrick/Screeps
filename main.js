6677e96829da033ab4a89bff1d06c3ac9849b88c
Matthew Swarbrick <matt_swarbrick@hotmail.com>
2017-06-16 13:59:05 +0100

    added some upgraders to upgrade the room controller

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 var roleHarvester = require("./role.harvester");
+var roleUpgrader = require("./role.upgrader");
 var SpawnName = 'Origins';
 module.exports = {
     loop: function () {
@@ -10,17 +11,24 @@ module.exports = {
             }
         }
         var harvesters = _.filter(Game.creeps, function (creep) { return creep.memory.role == 'harvester'; });
+        var upgraders = _.filter(Game.creeps, function (creep) { return creep.memory.role == 'upgrader'; });
         if (harvesters.length < 5) {
             var newName = Game.spawns[SpawnName].createCreep([WORK, CARRY, MOVE], undefined, { role: 'harvester' });
         }
+        if (upgraders.length < 5) {
+            var newName = Game.spawns[SpawnName].createCreep([WORK, CARRY, MOVE], undefined, { role: 'upgrader' });
+        }
         if (Game.spawns[SpawnName].spawning) {
             var spawningCreep = Game.creeps[Game.spawns[SpawnName].spawning.name];
-            Game.spawns[SpawnName].room.visual.text('🛠️' + spawningCreep.memory.role, Game.spawns[SpawnName].pos.x + 1, Game.spawns[SpawnName].pos.y, { align: 'left', opacity: 0.8 });
+            Game.spawns[SpawnName].room.visual.text('Building ' + spawningCreep.memory.role, Game.spawns[SpawnName].pos.x + 1, Game.spawns[SpawnName].pos.y, { align: 'left', opacity: 0.8 });
         }
         for (var name in Game.creeps) {
             var creep = Game.creeps[name];
             if (creep.memory.role == 'harvester') {
                 roleHarvester.run(creep);
+            }
+            if (creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
             }
         }
     }
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import roleHarvester = require('./role.harvester');
+import roleUpgrader = require('./role.upgrader');
 
 const SpawnName:string = 'Origins';
 
@@ -13,15 +14,20 @@ export = {
         }
 
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
 
         if(harvesters.length < 5) {
             var newName = Game.spawns[SpawnName].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         }
 
+        if(upgraders.length < 5) {
+            var newName = Game.spawns[SpawnName].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        }
+
         if(Game.spawns[SpawnName].spawning) {
             var spawningCreep = Game.creeps[Game.spawns[SpawnName].spawning.name];
             Game.spawns[SpawnName].room.visual.text(
-                '🛠️' + spawningCreep.memory.role,
+                'Building ' + spawningCreep.memory.role,
                 Game.spawns[SpawnName].pos.x + 1,
                 Game.spawns[SpawnName].pos.y,
                 {align: 'left', opacity: 0.8});
@@ -31,6 +37,9 @@ export = {
             var creep = Game.creeps[name];
             if(creep.memory.role == 'harvester') {
                 roleHarvester.run(creep);
+            }
+            if(creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
             }
         }
     }
