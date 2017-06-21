89853980ad3ee7d9835ccca4f62279f82ef9d5d1
Matthew Swarbrick <matt_swarbrick@hotmail.com>
2017-06-21 14:47:59 +0100

    enabled harvesters to transfer engery to extensions and towers

--- role.harvester.ts
+++ role.harvester.ts
@@ -9,8 +9,17 @@ var roleHarvester = {
             }
         }
         else {
-            if(creep.transfer(Game.spawns[SpawnName], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns[SpawnName]);
+            var targets = creep.room.find<Structure>(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
 	}
